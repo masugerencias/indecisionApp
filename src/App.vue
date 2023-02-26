89a0976e0d790f44e07e32b73ff61cc6ebf4431b
Yann Poirot sanz <ypoirot@beonit.es>
2023-02-26 19:02:06 +0100

    fist commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Indecision/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Indecision from './components/Indecision.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Indecision
   }
 }
 </script>
---
+++ src/components/Indecision.vue
@@ -0,0 +1,8 @@
+<template>
+<h1>indecision component</h1>
+</template>
+<script>
+export default {
+
+}
+</script>
